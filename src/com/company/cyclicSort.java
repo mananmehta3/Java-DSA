e325ee46c5938349ed9730aea281164f7c4f61c6
mananmehta3 <mananmehta@outlook.com>
2021-09-09 17:09:32 +0530

    Initial commit

---
+++ src/com/company/cyclicSort.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Arrays;
+// import java.util.Scanner;
+
+public class cyclicSort {
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        int[] arr = {11,12,13,3,4,2,1,5,8,6,7,10,9};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]==arr[correct]) i++;
+            else{
+                int temp = arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+        }
+    }
+}
---
+++ src/com/company/strings.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class strings {
+
+    public static void main(String[] args) {
+//        String a = "Manan";
+//        String b = new String("Manan");
+//        System.out.println(a.equals(b));
+//        System.out.println(a == b);
+//        System.out.println("Nishika");
+//        // System.out.println(b.length() + " " + b.charAt(2));
+//        String name = null;
+//        System.out.println(name);
+//        int num = 7;
+//        System.out.println(Integer.toBinaryString(num));
+//        System.out.println(Arrays.toString(new int[]{}));
+//
+        // PrettyPrinting
+//         float a = 453.728739f;
+//         System.out.println(a);
+//         System.out.printf("%.2f",a);
+//        System.out.println();
+//        System.out.printf("%.3f",Math.PI);
+        // System.out.printf("My name is %s and I am %s cool","Manan","Super");
+//        System.out.println('a' + 'b');
+//        System.out.println("a" + "b" + new ArrayList<>());
+//        System.out.println((char)('a'+3));
+//        System.out.println("Kunal"+56+new ArrayList<>());
+
+        // Performance
+//        String str = ""; // o(n^2) complexity
+//        for (int i = 0; i < 26; i++) {
+//            char ch = (char) ('a' + i);
+//            str += ch;
+//        }
+//        StringBuilder str = new StringBuilder();
+//        for (int i = 0; i < 26; i++) {
+//            char ch = (char) ('a' + i);
+//            str.append(ch);
+//        }
+//        System.out.println(str.toString());
+//        StringBuilder n = new StringBuilder();
+//        String name = "Manan";
+//        n.append(name);
+//        System.out.println(str.deleteCharAt(3));
+//        System.out.println(n.lastIndexOf("a"));
+
+    // Methods
+        String name = "Manan Mehta";
+        name = name.toUpperCase();
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+}
