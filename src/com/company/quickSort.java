d403fa8cd4ed87bee290af5f091afd64cd9248c0
mananmehta3 <mananmehta@outlook.com>
2021-10-20 17:01:12 +0530

    Learned Quick Sort

---
+++ src/com/company/quickSort.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class quickSort {
+    public static void main(String[] args) {
+        int[] arr = {24, 235, 24, 5, 2, 3, 25, -34, 25, 52, 0, -525, -25};
+        quickSortFunc(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quickSortFunc(int[] arr, int l, int h) {
+        if (l > h) return;
+        int s = l;
+        int e = h;
+        int m = s + (e - s) / 2;
+        int pivot = arr[m];
+        while(s<=e){
+            while(arr[s]<pivot) s++;
+            while(arr[e]>pivot) e--;
+            if(s<=e){
+                int temp = arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;
+                e--;
+            }
+        }
+        quickSortFunc(arr,l,e);
+        quickSortFunc(arr,s,h);
+    }
+}
