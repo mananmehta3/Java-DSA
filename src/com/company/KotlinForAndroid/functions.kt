83e4d0cb08f91856cd8b7d95140164f9125ea996
mananmehta3 <mananmehta@outlook.com>
2021-10-31 16:12:45 +0530

    Learned Basics Of Kotlin for Android Development

---
+++ src/com/company/KotlinForAndroid/functions.kt
@@ -0,0 +1,9 @@
+package com.company.KotlinForAndroid
+
+fun awesomeFun(name: String): String {
+    return name + " is awesome!!"
+}
+
+fun main() {
+    println(awesomeFun("Dairy Milk Silk"))
+}
---
+++ src/com/company/KotlinForAndroid/variables.kt
@@ -0,0 +1,10 @@
+package com.company.KotlinForAndroid
+
+fun main(args: Array<String>) {
+    var x: Float = 10F
+    x = 20.43F
+    var y: Int = x.toInt()
+    y++
+    --y
+    println("Hello World " + y)
+}
